76c5a5c0f90862f1738d62ec5bef93c6658b7bbc
firebynightgames <firebynightgames@gmail.com>
2026-05-27 08:27:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ const character = {
 /* ================================================
    STORAGE KEY
    ================================================ */
-const STORAGE_KEY = "fromRuinCharacter_v6";
+const STORAGE_KEY = "fromRuinCharacter_v10";
 
 /* ================================================
    PAIR-ENGINE CHECKBOX GUARD
